13ff5cc42fe3e8c0e3e0d984b878383f39f5bd6b
Rolf Dergham <rolf.dergham.public@gmail.com>
2021-08-26 21:43:46 +0300

    fix routing and tests

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { RouterModule } from '@angular/router';
+
+import { NgLandingComponent } from './ng-landing/app.component';
 import { AppComponent } from './app.component';
-import { MyCompComponent } from './my-comp/my-comp.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyCompComponent
+    NgLandingComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'home', component: HomeComponent},
+      {path: 'ng-landing', component: NgLandingComponent},
+      { path: '', redirectTo: '/home', pathMatch: 'full' },
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- my-app/src/app/ng-landing/app.component.spec.ts
+++ my-app/src/app/ng-landing/app.component.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { NgLandingComponent as AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -16,19 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-
-  /*
   it(`should have as title 'my-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('my-app');
   });
+
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain('my-app app is running!');
   });
-  */
-  
 });
